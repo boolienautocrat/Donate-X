b441784d00ea6d629d3fb2bb7ec30158be95718a
Jose <josetsaji2006@gmail.com>
2022-07-15 22:39:52 +0530

    added hamburger menu functionality

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-var header = document.querySelector("header")
+const header = document.querySelector("header")
+const hamBtn = document.querySelector(".ham-btn")
+const hamIcon = document.querySelector(".ham-btn img")
+const hamMenu = document.querySelector(".ham-menu")
+const body = document.querySelector("body")
 
 function navColor() {
     if (window.scrollY > 150) {
@@ -9,3 +13,19 @@ function navColor() {
 }
 
 window.addEventListener("scroll", navColor)
+
+hamBtn.onclick = () => {
+    if (hamBtn.classList.contains("ham-btn")) {
+        hamBtn.classList.remove("ham-btn")
+        hamBtn.classList.add("close-btn")
+        hamIcon.src = "./images/close-menu.svg"
+        hamMenu.style.display = "block";
+        body.style.overflowY = "hidden"
+    } else if (hamBtn.classList.contains("close-btn")) {
+        hamBtn.classList.remove("close-btn")
+        hamBtn.classList.add("ham-btn")
+        hamIcon.src = "./images/hamburger-menu.svg"
+        hamMenu.style.display = "none";
+        body.style.overflowY = "scroll"
+    }
+}
